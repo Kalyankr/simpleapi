201f4ab10e964e6428ae09dbe68cf1e7c45d8760
kalyankr <kalyan.katla@gmail.com>
2025-01-13 14:27:34 -0600

    efficent request module

--- simpleapi/request.py
+++ simpleapi/request.py
@@ -1,22 +1,17 @@
 from collections import defaultdict
+from urllib.parse import parse_qs
 
 
 class Request:
     def __init__(self, environ) -> None:
         self.environ = environ
-        self.queries = defaultdict()
         self.headers = self._parse_headers()
+        self.queries = self._parse_query_string()
 
         # Set attributes for all keys in environ
         for key, value in self.environ.items():
+            # Convert WSGI keys to a standard format for easier access
             setattr(self, key.replace(".", "_").lower(), value)
-
-        # Parse query string into queries dictionary
-        if self.query_string:
-            req_queries = self.query_string.split("&")
-            for query in req_queries:
-                query_key, query_val = query.split("=")
-                self.queries[query_key] = query_val
 
     def _parse_headers(self):
         """Extract headers from the WSGI environ."""
@@ -27,3 +22,16 @@ class Request:
                 header_name = key[5:].replace("_", "-").title()
                 headers[header_name] = value
         return headers
+
+    def _parse_query_string(self):
+        """Parse the query string into a dictionary using urllib."""
+        queries = defaultdict(str)
+        query_string = self.environ.get("QUERY_STRING", "")
+        if query_string:
+            parsed_queries = parse_qs(query_string)
+            # Use `parse_qs` to ensure proper decoding
+            for key, value in parsed_queries.items():
+                queries[key] = value[
+                    0
+                ]  # Only take the first value if there are multiple
+        return queries
